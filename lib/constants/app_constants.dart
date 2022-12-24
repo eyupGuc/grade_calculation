095dc8d326c596081aff8a86f77aa59ceee742a5
eyupGuc <meydanguresi@gmail.com>
2022-12-24 06:33:50 +0300

    added gif

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -12,7 +12,7 @@ class Sabitler {
       fontSize: 16, fontWeight: FontWeight.w600, color: anaRenk);
 
   static final TextStyle ortalamaStyle = GoogleFonts.quicksand(
-      fontSize: 55, fontWeight: FontWeight.w800, color: anaRenk);
+      fontSize: 30, fontWeight: FontWeight.w800, color: anaRenk);
 
   static final dropDownPadding =
       EdgeInsets.symmetric(horizontal: 16, vertical: 8);
